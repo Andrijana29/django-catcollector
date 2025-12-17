b494ecceda1bd2c95300fa6df4b2f1f1da558f5a
Andrijana29 <andrijanamarkovic16@gmail.com>
2025-12-17 14:45:38 +0100

    commit

---
+++ main_app/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    colour = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0])
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    # Create a cat_id FK
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
+
+
+    
